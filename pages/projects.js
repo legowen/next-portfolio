9aee2de92603e7a41240cf13c976bcc34cb603b1
OwenSyKim <owen.sykim@gmail.com>
2024-01-29 23:39:27 -0800

    Add Notion API / .env

--- pages/projects.js
+++ pages/projects.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Layout from "@/components/layout";
 import Head from "next/head";
+import { TOKEN, DATABASE_ID } from "@/config";
 
 export default function projects() {
   return (
@@ -10,9 +11,38 @@ export default function projects() {
         <meta name="description" content="legowen" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-        My Projects
-
+      My Projects
     </Layout>
   );
+}
+
+//Build Time Request
+export async function getStaticProps() {
+  const options = {
+    method: "POST",
+    headers: {
+      accept: "application/json",
+      "Notion-Version": "2022-06-28",
+      "content-type": "application/json",
+      Authorization: `Bearer ${TOKEN}`,
+    },
+    body: JSON.stringify({ page_size: 100 }),
+  };
+
+  const res = await fetch(
+    `https://api.notion.com/v1/databases/${DATABASE_ID}/query`,
+    options
+  );
+
+  const projects = await res.json();
+
+  console.log(projects);
+
+  const projectsIds = projects.results.map((aProject) => aProject.id);
+
+  console.log("projectIds : ${projectIds}");
+
+  return {
+    props: {},
+  };
 }
