7f58b2344e227003d808a7389aa14a36018716c0
OwenSyKim <owen.sykim@gmail.com>
2024-02-13 17:25:12 -0800

    Update

--- pages/projects.js
+++ pages/projects.js
@@ -9,16 +9,23 @@ export default function projects({ projects }) {
 
   return (
     <Layout>
-      <Head>
-        <title>Legowen</title>
-        <meta name="description" content="legowen" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <h1>My Projects : {projects.results.length} </h1>
+      <div className="flex flex-col items-center justify-center min-h-screen px-5 mb-10 px-6">
+        <Head>
+          <title>Legowen</title>
+          <meta name="description" content="legowen" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <h1 className="text-4xl font-bold sm:text-6xl">
+          My Projects : 
+          <span className="pl-4 text-blue-500">{projects.results.length}</span>
+        </h1>
 
-      {projects.results.map((aProject) => (
-        <ProjectItem key={aProject.id} data={aProject}/>
-      ))}
+        <div className="grid grid-cols-1 md:grid-cols-2 py-10 m-6 gap-8 w-full">
+          {projects.results.map((aProject) => (
+            <ProjectItem key={aProject.id} data={aProject} />
+          ))}
+        </div>
+      </div>
     </Layout>
   );
 }
@@ -34,14 +41,14 @@ export async function getStaticProps() {
       Authorization: `Bearer ${TOKEN}`,
     },
     body: JSON.stringify({
-        sorts: [
-            {
-                "property": "이름",
-                "direction":"ascending"
-
-        }
-    ],
-        page_size: 100 }),
+      sorts: [
+        {
+          property: "이름",
+          direction: "ascending",
+        },
+      ],
+      page_size: 100,
+    }),
   };
 
   const res = await fetch(
